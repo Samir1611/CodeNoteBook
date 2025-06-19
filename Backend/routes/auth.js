af4c89abc73c25ed3640378952d2fff8cf01a42a
samir1611 <johnshakya16@gmail.com>
2025-06-19 15:12:57 +0545

    adjusted

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -142,8 +142,8 @@ router.post(
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production", // true in production (HTTPS)
-          sameSite: "Lax", // CSRF protection
+          secure: true, // true in production (HTTPS)
+          sameSite: "none", // CSRF protection
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         })
         .json({
